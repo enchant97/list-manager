583c59c2a3e91a5bd8e28ae6fc9e4f30c7ea107d
Leo Spratt <enchant97@users.noreply.github.com>
2022-02-01 13:55:21 +0000

    set default sse ping to 60secs

--- backend/list_manager_api/live_update.py
+++ backend/list_manager_api/live_update.py
@@ -9,10 +9,10 @@ router = APIRouter(dependencies=(Depends(verify_api_key),))
 
 @router.get("/lists", response_class=EventSourceResponse)
 async def all_updates(request: Request):
-    return EventSourceResponse(sse_message_publisher(request=request, list_id=None))
+    return EventSourceResponse(sse_message_publisher(request=request, list_id=None), ping=60)
 
 
 @router.get("/lists/{list_id}", response_class=EventSourceResponse)
 async def specific_list_updates(list_id: int, request: Request):
     await crud.read_item_list(list_id)
-    return EventSourceResponse(sse_message_publisher(request=request, list_id=list_id))
+    return EventSourceResponse(sse_message_publisher(request=request, list_id=list_id), ping=60)
